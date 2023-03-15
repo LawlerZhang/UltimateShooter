5bdd11837cc8234e01e56f87f394a547a4126d75
LawlerZhang <shz1996@mail.ustc.edu.cn>
2023-03-15 21:01:45 +0800

    initialize project

---
+++ Source/UltimateShooter/UltimateShooterGameModeBase.h
@@ -0,0 +1,18 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UltimateShooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+
+UCLASS()
+class ULTIMATESHOOTER_API AUltimateShooterModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
